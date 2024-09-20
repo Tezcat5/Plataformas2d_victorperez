314f31aa66ccc43fa0afc43e34ef56d4ab67b980
Tezcat5 <vicpero96@gmail.com>
2024-09-20 10:51:11 +0200

    Salto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D characterRigidbody;
     private float horizontalInput;
+    private bool jumpInput;
     [SerializeField]private float characterSpeed = 4.5f;
-    [SerializeField]private float jumpForce = 5f;
+    [SerializeField]private float jumpForce = 10f;
 
     void Awake()
     {
@@ -17,12 +18,17 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //characterRigidbody.AddForce(Vector2.up * jumpForce);
     }
 
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            characterRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 
     // Update is called once per frame
