e52e696b1c10036461142b6e0e762e27f2f0e7e2
Tezcat5 <vicpero96@gmail.com>
2024-10-04 12:07:12 +0200

    MovimientoArreglado

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -64,26 +64,45 @@ public class PlayerController : MonoBehaviour
 
         if(isAttacking)
         {
-
-        }
-        horizontalInput = Input.GetAxis("Horizontal");
-
-        if(horizontalInput < 0)
-        {
-            transform.rotation = Quaternion.Euler(0, 180, 0);
-            characterAnimator.SetBool("IsRunning", true);
-        }
-        else if(horizontalInput > 0)
-        {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-            characterAnimator.SetBool("IsRunning", true);
+            horizontalInput = 0;
         }
         else
         {
-            characterAnimator.SetBool("IsRunning",false);
+            horizontalInput = Input.GetAxis("Horizontal");
         }
+
+        if(horizontalInput < 0)
+    {
+        if(!isAttacking)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        characterAnimator.SetBool("IsRunning", true);
     }
-    
+    else if(horizontalInput > 0)
+    {
+        if(!isAttacking)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        characterAnimator.SetBool("IsRunning", true);
+    }
+    else
+    {
+        characterAnimator.SetBool("IsRunning", false);
+    }
+}
+
+
+
+
+
+
+
+
+
+
     void Jump()
     {
         characterRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
